b1174ae88ce7bf32ca11aaac4130716c68b18b71
AlanDagner <alanquenaya@upeu.edu.pe>
2024-08-03 18:03:35 -0500

    Search and Filter

--- resources/views/documentos/index.blade.php
+++ resources/views/documentos/index.blade.php
@@ -260,9 +260,9 @@
             </div>
         </div>
     </div>
-    {{-- <div class="d-flex justify-content-center mt-4">
+    <div class="d-flex justify-content-center mt-4">
         {{ $documentos->links() }}
-    </div> --}}
+    </div>
 </div>
 
 <script>
